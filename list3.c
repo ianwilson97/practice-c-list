d777dcd0503373508705ff00d2952b4e53daaeaf
ianwilson97 <ianwilsonarmand0909@gmail.com>
2022-07-23 10:09:02 -0400

    Changes made

--- list3.c
+++ list3.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-// this is for inserting a node at the nth node
+// this is file is for inserting a node at position n in a linked list
+
 struct Node {
-    int data;
+    int data; 
     struct Node *next;
 };
 struct Node* head; //this is the head of the list
@@ -36,15 +37,9 @@ void Print() {
 int main()
 {
     head = NULL; //this is currently an empty list
-    printf("How many numbers?\n"); //this is asking the user how many numbers they want to enter
-    int n, i, x; //n is the number of numbers they want to enter. i is the counter. x is the number that is being entered
-    scanf("%d", &n); //this is taking the number of numbers the user wants to enter
-    for(i = 0; i < n; i++) {
-        printf("Enter a number: \n"); //this is asking the user to enter a number
-        scanf("%d", &x); //this is taking the number that the user wants to enter
-        Insert(x, i+1); //this is calling the function Insert
-        Print(); //this calls the function Print to print the list
-    }
-    
+    Insert(2, 1);
+    Insert(3, 2);
+    Insert(4, 1);
+    Insert(5, 2)
     return 0;
 }
